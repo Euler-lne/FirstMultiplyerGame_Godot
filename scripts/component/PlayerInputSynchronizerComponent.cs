6ae3a975ec462b46154ab938f68c9fd3e540a7cf
Euler-lne <319512122@qq.com>
2026-01-12 12:03:41 +0800

    实现武器同步旋转

--- scripts/component/PlayerInputSynchronizerComponent.cs
+++ scripts/component/PlayerInputSynchronizerComponent.cs
@@ -3,7 +3,9 @@ using System;
 
 public partial class PlayerInputSynchronizerComponent : MultiplayerSynchronizer
 {
+	[Export] private Node2D aimRoot;
 	private Vector2 movementVector = Vector2.Zero; // [Export] 是为了在Godot中选中这个变量进行同步，不写可以直接写变量名
+	private Vector2 aimVector = Vector2.Right;
 
 
 	public override void _Process(double delta)
@@ -17,6 +19,8 @@ public partial class PlayerInputSynchronizerComponent : MultiplayerSynchronizer
 	private void GatherInput()
 	{
 		movementVector = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		aimVector = aimRoot.GlobalPosition.DirectionTo(aimRoot.GetGlobalMousePosition()); // 只有Node2D才有GetGlobalMousePosition属性
+
 	}
 
 	public Vector2 GetMovementVector()
@@ -24,4 +28,9 @@ public partial class PlayerInputSynchronizerComponent : MultiplayerSynchronizer
 		return movementVector;
 	}
 
+	public Vector2 GetAimVector()
+	{
+		return aimVector;
+	}
 }
+
--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -4,19 +4,41 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export] private PlayerInputSynchronizerComponent synchronizer;
-	public const float Speed = 150.0f;
+	[Export] private Node2D weaponRoot;
+	public const float Speed = 100.0f;
+
+	// private Vector2 startDrawPosition = Vector2.Zero;
+	// private Vector2 endDrawPosition = Vector2.Zero;
+	// private Vector2 aimVector = Vector2.Right;
+
+	public override void _Draw()
+	{
+		// DrawLine(startDrawPosition, endDrawPosition, Colors.Red, 2f);
+		// DrawLine(Vector2.Zero, aimVector * 50, Colors.Green, 2f);
+	}
+
 
 	public override void _Ready()
 	{
 		// GD.Print("Player " + Name + " ready. My ID is " + Multiplayer.GetUniqueId() + ". Am I Authority " + IsMultiplayerAuthority());
-		SetProcess(IsMultiplayerAuthority());  // 只有拥有 Multiplayer Authority 的节点才会执行 _Process 方法,也就是只有服务器才会执行
+		// SetProcess(IsMultiplayerAuthority());  // 只有拥有 Multiplayer Authority 的节点才会执行 _Process 方法,也就是只有服务器才会执行
 		synchronizer.SetMultiplayerAuthority(int.Parse(Name));
 	}
 
 
 	public override void _Process(double delta)
 	{
-		Velocity = synchronizer.GetMovementVector() * Speed;
-		MoveAndSlide();
+		weaponRoot.Rotation = synchronizer.GetAimVector().Angle();
+		// 只有在服务器上执行移动，武器的朝向在各自的客户端上执行
+		if (IsMultiplayerAuthority())
+		{
+			Velocity = synchronizer.GetMovementVector() * Speed;
+			MoveAndSlide();
+		}
+
+		// startDrawPosition = weaponRoot.GlobalPosition;
+		// endDrawPosition = GetGlobalMousePosition();
+		// aimVector = startDrawPosition.DirectionTo(endDrawPosition);
+		// QueueRedraw();
 	}
 }
